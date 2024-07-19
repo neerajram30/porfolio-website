5618eecb09e49d6bb467bb6f47a869a9e32aa89c
neeraj <neerajramachandran30@gmail.com>
2024-07-19 20:48:12 +0530

    feat: add meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
 export const metadata = {
   title: "Neeraj MR | Full stack developer",
   description: "Neeraj's developer portfolio",
+  ogImage: '/portfolio.png'
 };
 
 export default async function RootLayout({ children }) {
