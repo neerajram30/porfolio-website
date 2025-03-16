484bc8715faa3c4464a9e5ddea3b5851b0af9004
neeraj <neerajramachandran30@gmail.com>
2025-03-17 00:04:32 +0530

    fix:meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,8 @@ const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
 export const metadata = {
   title: "Neeraj MR | Full stack developer",
   description: "Neeraj's developer portfolio",
-  ogImage: 'https://neerajram.netlify.app/portfolio.png'
+  ogImage: 'https://neerajram.netlify.app/portfolio.png',
+  verification: { google: "SS5GO0NWJv7wWHeWAgKj--lo-fU-WiuBrogioNJbUnM" },
 };
 
 export default async function RootLayout({ children }) {
--- src/app/page.js
+++ src/app/page.js
@@ -21,13 +21,13 @@ export default function Home() {
   }, []);
   return (
     <>
-      <Head>
+      {/* <Head>
         <meta
           name="google-site-verification"
           content="SS5GO0NWJv7wWHeWAgKj--lo-fU-WiuBrogioNJbUnM"
         />
          <meta name="description" content="Neeraj M R React developer"/>
-      </Head>
+      </Head> */}
       <main className="flex flex-col items-start overflow-x-hidden">
         {!loading ? (
           <>
