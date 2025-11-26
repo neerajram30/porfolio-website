3da629978a3601d94dbbf12c068ebcd1f43c0a73
neeraj <neerajramachandran30@gmail.com>
2025-11-26 18:55:38 +0530

    enhancement: resume link change

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -137,7 +137,7 @@ function Profile() {
           <Link
             target="_blank"
             type="submit"
-            href="https://drive.google.com/file/d/1GbmgU_YQuA5fDJVQBUtyDIoBr56RfeKM/view"
+            href="https://drive.google.com/file/d/1dm5sVqpcV3RmzHhegYTm0v69rxJnrvDX/view"
           >
             <button className="mb-5 bg-primary h-10 w-28 rounded-sm text-white  hover:border-white font-overpass font-semibold text-sm">
               Get resume
